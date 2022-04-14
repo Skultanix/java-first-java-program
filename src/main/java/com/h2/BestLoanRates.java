fe64a4e0d73102b2ee819dc4b39424d98268fb70
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-14 15:41:50 -0600

    Initial commit.

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,18 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello "+name);
+    }
+
+    public static final Map<Integer, Float> bestRates() {
+        Map.of();
+        return null;
+    }
+}
